99537b7da911b51ef628780d7ce5151be347e002
Rajendra Wagle <raajen73@gmail.com>
2021-08-16 22:11:51 +0545

    Code checked with phpcs

--- src/Provider/Dropbox.php
+++ src/Provider/Dropbox.php
@@ -12,156 +12,156 @@ use Raajen\OAuth2\Dropbox\Client\Exception\NoStateException;
 
 class Dropbox extends AbstractProvider
 {
-	use BearerAuthorizationTrait;
+    use BearerAuthorizationTrait;
 
-	/**
-	 * @var string Sets the state to be checked to prevent CSRF.
-	 */
-	protected $state = null;
+    /**
+     * @var string Sets the state to be checked to prevent CSRF.
+     */
+    protected $state = null;
 
-	/**
-	 * @var array Stores the extra information passed to the auth url.
-	 */
-	protected $args = [];
+    /**
+     * @var array Stores the extra information passed to the auth url.
+     */
+    protected $args = [];
 
-	/**
-	 * @var string Key used in the access token response to identify the resource owner.
-	 */
-	const ACCESS_TOKEN_RESOURCE_OWNER_ID = 'account_id';
+    /**
+     * @var string Key used in the access token response to identify the resource owner.
+     */
+    public const ACCESS_TOKEN_RESOURCE_OWNER_ID = 'account_id';
 
-	/**
-	 * @var string API authorization URL.
-	 */
-	const API_AUTH_URL = 'https://www.dropbox.com/oauth2/authorize';
+    /**
+     * @var string API authorization URL.
+     */
+    public const API_AUTH_URL = 'https://www.dropbox.com/oauth2/authorize';
 
-	/**
-	 * @var string Base authorization token URL.
-	 */
-	const API_BASE_URL = 'https://api.dropbox.com';
+    /**
+     * @var string Base authorization token URL.
+     */
+    public const API_BASE_URL = 'https://api.dropbox.com';
 
-	/**
-	 * @var array List of allowed arguments that can be set.
-	 */
-	protected $allowed = [
-		'token_access_type',
-		'approval_prompt'
-	];
+    /**
+     * @var array List of allowed arguments that can be set.
+     */
+    protected $allowed = [
+        'token_access_type',
+        'approval_prompt'
+    ];
 
-	/**
-	 * Base authorization url.
-	 *
-	 * @return string
-	 */
-	public function getBaseAuthorizationUrl()
-	{
-		return Dropbox::API_AUTH_URL;
-	}
+    /**
+     * Base authorization url.
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl()
+    {
+        return Dropbox::API_AUTH_URL;
+    }
 
-	/**
-	 * Access token base URL.
-	 *
-	 * @param array $params
-	 * @return string
-	 */
-	public function getBaseAccessTokenUrl(array $params = array())
-	{
-		$base = Dropbox::API_BASE_URL;
-		return "{$base}/oauth2/token";
-	}
+    /**
+     * Access token base URL.
+     *
+     * @param array $params
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params = array())
+    {
+        $base = Dropbox::API_BASE_URL;
+        return "{$base}/oauth2/token";
+    }
 
-	/**
-	 * Resource owner details url.
-	 *
-	 * @param AccessToken $token
-	 * @return string
-	 */
-	public function getResourceOwnerDetailsUrl(AccessToken $token = null)
-	{
-		$base = Dropbox::API_BASE_URL;
-		return "{$base}/2/users/get_current_account";
-	}
+    /**
+     * Resource owner details url.
+     *
+     * @param AccessToken $token
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token = null)
+    {
+        $base = Dropbox::API_BASE_URL;
+        return "{$base}/2/users/get_current_account";
+    }
 
-	/**
-	 * Set the state.
-	 *
-	 * @param string $state
-	 *
-	 * @return void
-	 */
-	public function setState($state = '')
-	{
-		if ('' === $state) {
-			$state = $this->getRandomState(32);
-		}
+    /**
+     * Set the state.
+     *
+     * @param string $state
+     *
+     * @return void
+     */
+    public function setState($state = '')
+    {
+        if ('' === $state) {
+            $state = $this->getRandomState(32);
+        }
 
-		$this->state = $state;
-	}
+        $this->state = $state;
+    }
 
-	/**
-	 * Sets the argument.
-	 *
-	 * @throws InvalidArgumentException
-	 *
-	 * @param string $key
-	 * @param mixed  $value
-	 *
-	 * @return void
-	 */
-	public function set($key, $value)
-	{
-		if (!in_array($key, $this->allowed)) {
-			throw new InvalidArgumentException("The provided key: {$key} is not allowed");
-		}
+    /**
+     * Sets the argument.
+     *
+     * @throws InvalidArgumentException
+     *
+     * @param string $key
+     * @param mixed  $value
+     *
+     * @return void
+     */
+    public function set($key, $value)
+    {
+        if (!in_array($key, $this->allowed)) {
+            throw new InvalidArgumentException("The provided key: {$key} is not allowed");
+        }
 
-		$this->args[$key] = $value;
-	}
+        $this->args[$key] = $value;
+    }
 
-	/**
-	 * Get all the arguments.
-	 *
-	 * @return array
-	 */
-	public function getArgs()
-	{
-		return $this->args;
-	}
+    /**
+     * Get all the arguments.
+     *
+     * @return array
+     */
+    public function getArgs()
+    {
+        return $this->args;
+    }
 
-	/**
-	 * Get the value from the args.
-	 *
-	 * @param string $key
-	 *
-	 * @return mixed
-	 */
-	public function get($key)
-	{
-		if (!in_array($key, $this->args)) {
-			return '';
-		}
+    /**
+     * Get the value from the args.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        if (!in_array($key, $this->args)) {
+            return '';
+        }
 
-		return $this->args[$key];
-	}
+        return $this->args[$key];
+    }
 
-	/**
-	 * Generates the authorization URL.
-	 *
-	 * @throws NoStateException
-	 *
-	 * @param array $options
-	 * @return void
-	 */
-	public function getAuthorizationUrl(array $options = [])
-	{
-		if (null === $this->state) {
-			throw new NoStateException('Cannot find the state!');
-		}
+    /**
+     * Generates the authorization URL.
+     *
+     * @throws NoStateException
+     *
+     * @param array $options
+     * @return void
+     */
+    public function getAuthorizationUrl(array $options = [])
+    {
+        if (null === $this->state) {
+            throw new NoStateException('Cannot find the state!');
+        }
 
-		$default = array_merge($this->args, ['state' => $this->state]);
+        $default = array_merge($this->args, ['state' => $this->state]);
 
-		return parent::getAuthorizationUrl(array_merge($default, $options));
-	}
+        return parent::getAuthorizationUrl(array_merge($default, $options));
+    }
 
-	/**
+    /**
      * Get the default scopes used by this provider.
      *
      * This should not be a complete list of all scopes, but the minimum
@@ -169,18 +169,18 @@ class Dropbox extends AbstractProvider
      *
      * @return array
      */
-	protected function getDefaultScopes()
-	{
-		return [];
-	}
+    protected function getDefaultScopes()
+    {
+        return [];
+    }
 
-	/**
+    /**
      * Check a provider response for errors.
      *
      * @link   https://www.dropbox.com/developers/core/docs
-	 *
+     *
      * @throws IdentityProviderException
-	 *
+     *
      * @param  ResponseInterface $response
      * @param  string $data Parsed response data.
      * @return void
@@ -196,7 +196,7 @@ class Dropbox extends AbstractProvider
         }
     }
 
-	/**
+    /**
      * Generate an user object from a successful user details request.
      *
      * @param  object $response
@@ -208,19 +208,17 @@ class Dropbox extends AbstractProvider
         return new ResourceOwner($response);
     }
 
-	/**
-	 * Requests resource owner details.
-	 *
-	 * @param  AccessToken $token
-	 * @return mixed
-	 */
-	protected function fetchResourceOwnerDetails(AccessToken $token)
-	{
-		$url     = $this->getResourceOwnerDetailsUrl($token);
-		$request = $this->getAuthenticatedRequest(self::METHOD_POST, $url, $token);
+    /**
+     * Requests resource owner details.
+     *
+     * @param  AccessToken $token
+     * @return mixed
+     */
+    protected function fetchResourceOwnerDetails(AccessToken $token)
+    {
+        $url     = $this->getResourceOwnerDetailsUrl($token);
+        $request = $this->getAuthenticatedRequest(self::METHOD_POST, $url, $token);
 
-		return $this->getParsedResponse($request);
-	}
-
-
+        return $this->getParsedResponse($request);
+    }
 }
--- src/Provider/ResourceOwner.php
+++ src/Provider/ResourceOwner.php
@@ -7,62 +7,62 @@ use League\OAuth2\Client\Tool\ArrayAccessorTrait;
 
 class ResourceOwner implements ResourceOwnerInterface
 {
-	use ArrayAccessorTrait;
+    use ArrayAccessorTrait;
 
-	/**
-	 * Raw response
-	 *
-	 * @var array
-	 */
-	protected $response;
+    /**
+     * Raw response
+     *
+     * @var array
+     */
+    protected $response;
 
-	/**
-	 * Creates a new resource owner.
-	 *
-	 * @param array $response
-	 */
-	public function __construct(array $response = array())
-	{
-		$this->response = $response;
-	}
+    /**
+     * Creates a new resource owner.
+     *
+     * @param array $response
+     */
+    public function __construct(array $response = array())
+    {
+        $this->response = $response;
+    }
 
-	/**
-	 * Get resource owner id.
-	 *
-	 * @return string
-	 */
-	public function getId()
-	{
-		return $this->getValueByKey($this->response, 'account_id');
-	}
+    /**
+     * Get resource owner id.
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->getValueByKey($this->response, 'account_id');
+    }
 
-	/**
-	 * Get resource owner name.
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return $this->getValueByKey($this->response, 'name.display_name');
-	}
+    /**
+     * Get resource owner name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->getValueByKey($this->response, 'name.display_name');
+    }
 
-	/**
-	 * Get resource owner email.
-	 *
-	 * @return string
-	 */
-	public function getEmail()
-	{
-		return $this->getValueByKey($this->response, '');
-	}
+    /**
+     * Get resource owner email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->getValueByKey($this->response, '');
+    }
 
-	/**
-	 * Return all of the owner details available as an array.
-	 *
-	 * @return array
-	 */
-	public function toArray()
-	{
-		return $this->response;
-	}
+    /**
+     * Return all of the owner details available as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->response;
+    }
 }
